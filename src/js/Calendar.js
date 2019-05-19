425b5baa2fceb43f06e098dd277f56b843e1c6f6
yamnyk <notAunicorN2018>
2019-05-19 12:20:10 +0300

    calendar works from one class Calendar. Months switching, today btn

--- src/js/Calendar.js
+++ src/js/Calendar.js
@@ -1,22 +1,17 @@
 import $ from 'jquery'
 
 export default class Calendar {
-	constructor(root='#calendar', header='.cal-nav__month', prevBtn = '.switch-btn__prev', nextBtn = '.switch-btn__next') {
+	constructor(root='#calendar', header='.cal-nav__month', prevBtn = '.switch-btn__prev', nextBtn = '.switch-btn__next', todayBtn = '.cal-nav__today') {
 		this.$root = $(root);
 		this.$header = $(header);
 		this.$nextBtn = $(nextBtn);
 		this.$prevBtn = $(prevBtn);
+		this.$todayBtn = $(todayBtn);
 		this.date = {
 			year: new Date().getFullYear(),
 			month: new Date().getMonth(),
 			day: new Date().getDate()
 		};
-		
-		this.lastOfMonth = new Date(this.date.year, this.date.month+1, 0);
-		this.lastWeekDay = this.lastOfMonth.getDay();
-		this.daysInMonth = this.lastOfMonth.getDate();
-		this.firstOfMonth = new Date(this.date.year, this.date.month, 1);
-		this.firstWeekDay = this.firstOfMonth.getDay();
 		
 		this.weekDays = [
 			'Воскресенье',
@@ -46,25 +41,32 @@ export default class Calendar {
 		
 		this.calendar= [];
 		
-		// this.$prevBtn.on('click', this.prevBtn, this);
-		// this.$nextBtn.on('click', this.nextBtn, this);
+		this.$prevBtn.on('click', $.proxy(this.prevBtn,this));
+		this.$nextBtn.on('click', $.proxy(this.nextBtn, this));
+		this.$todayBtn.on('click', $.proxy(this.goToday, this));
 		
 		// this.render()
 	}
 	
 	render() {
+		this.$root.empty();
+		
+		this.lastOfMonth = new Date(this.date.year, this.date.month+1, 0);
+		this.lastWeekDay = this.lastOfMonth.getDay();
+		this.daysInMonth = this.lastOfMonth.getDate();
+		this.firstOfMonth = new Date(this.date.year, this.date.month, 1);
+		this.firstWeekDay = this.firstOfMonth.getDay();
 		
 		this.$header.text(`${this.months[this.date.month]} ${this.date.year}`);
-		
 		this.createDaysHeaders();
 		
 		const $fragment = $(document.createDocumentFragment());
 		
 		if(this.firstWeekDay !== 0) {
 			this.addEmptyCells(1, this.firstWeekDay, $fragment);
-		} else if(firstWeekDay === 1) {
+		} else if(this.firstWeekDay === 1) {
 			this.addEmptyCells(0, this.firstWeekDay, $fragment);
-		} else if (firstWeekDay === 0) {
+		} else if (this.firstWeekDay === 0) {
 			this.addEmptyCells(this.firstWeekDay, 6, $fragment);
 		}
 		
@@ -76,7 +78,6 @@ export default class Calendar {
 		if(this.lastWeekDay !== 7 && this.lastWeekDay !== 0) {
 			this.addEmptyCells(this.lastWeekDay, 7, $fragment);
 		}
-		
 		
 		this.$root.append($fragment);
 	}
@@ -102,7 +103,7 @@ export default class Calendar {
 	
 	createDay(year, month, day) {
 		const dayNode = $(`
-		<div class="calendar__item${this.date.year === year && this.date.month === month && this.date.day === day? ' calendar__today' : ''}">
+		<div class="calendar__item${new Date().getFullYear() === year && new Date().getMonth()=== month && new Date().getDate() === day? ' calendar__today' : ''}">
 			<p class="day-num">${day}</p>
 		</div>
 		`);
@@ -130,19 +131,28 @@ export default class Calendar {
 	
 	prevBtn() {
 		if(this.date.month === 0) {
-			this.date.month -= 1;
+			this.date.year -= 1;
 		}
 		this.date.month = this.date.month === 0 ? 11 : Math.abs(this.date.month - 1);
+		this.render();
 		
-		this.render('#calendar','.cal-nav__month',this.date.year,this.date.month);
 	};
 	
 	nextBtn() {
-		if(this.date.month === 0) {
-			this.date.month += 1;
+		if(this.date.month === 11) {
+			this.date.year += 1;
 		}
-		this.date.month = this.date.month === 0 ? 11 : Math.abs(this.date.month + 1);
+		this.date.month = this.date.month === 11 ? 0 : Math.abs(this.date.month + 1);
 		
-		this.render('#calendar','.cal-nav__month',this.date.year,this.date.month);
+		this.render();
 	};
+	
+	goToday() {
+		this.date = {
+			year: new Date().getFullYear(),
+			month: new Date().getMonth(),
+			day: new Date().getDate()
+		};
+		this.render()
+	}
 }
