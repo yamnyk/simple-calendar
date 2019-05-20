eb592c1eed395a5579c57165460e5b6d209bfc4f
panteleymonchuk <panteleymonchuk@gmail.com>
2019-05-20 20:54:33 +0300

    Added packages for work with scss, changed webpack config (need improve),

--- webpack.config.js
+++ webpack.config.js
@@ -12,95 +12,147 @@ const NotifierPlugin = require('friendly-errors-webpack-plugin');
 const notifier = require('node-notifier');
 
 module.exports = {
-	entry: {
-		'css/style': styles,
-		'js/index': script
-	},
+	entry: [
+		'./src/js/index.js',
+		'./src/scss/style.scss'
+	],
 	output:{
 		filename:"[name].js",
-		path: path.resolve(__dirname,"dist")
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "dist/"
 	},
-	devServer: {
-		quiet: true,
-		overlay: true
-	},
-	optimization: {
-		minimizer: [
-			new TerserPlugin(),
-			new OptimizeCSSAssetsPlugin({ cssProcessorOptions: { map: { inline: false, annotation: true}}})
-		]
-	},
+	devtool: 'source-map',
 	module: {
 		rules: [
 			{
 				test: /\.html$/,
-				use: [{ loader: "html-loader", options: { minimize: true } }]
+				use: [{ loader: "html-loader"}],
 			},
 			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				loader: "url-loader",
-			},
-			{
-				test: /\.m?js$/,
+				test: /\.js$/,
 				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
+					loader: "babel-loader",
+					options: { presets: ["@babel/preset-env"] }
 				}
 			},
+			// TODO improve work with scss using minify, uglify, etc.
 			{
 				test: /\.scss$/,
 				use: [
-					{loader: "style-loader"},
-					{loader: MiniCssExtractPlugin.loader},
-					{loader: "css-loader"},
-					{loader: "postcss-loader", options: {
-							sourceMap: true,
-							plugins: () => [
-								require('autoprefixer')({
-									'browsers': ['> 0.2%', 'last 100 versions']
-								}),
-								require('css-mqpacker'),
-								require('cssnano')({
-									preset: [
-										'default',
-										{
-											discardComments: {
-												removeAll: true
-											}
-										}
-									]
-								})]
-						}},
-					{loader: "sass-loader"},
-				
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].css'
+						}
+					},
+					'extract-loader',
+					'css-loader',
+					'sass-loader',
 				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loader: "file-loader",
 			}
 		]
 	},
+	resolve: {
+		extensions: ['.js', '.scss']
+	},
 	plugins: [
-		new HtmlWebPackPlugin({
-			template: "src/index.html",
-			filename: "./index.html"
-		}),
 		new MiniCssExtractPlugin({
 			filename: "[name].css",
 			chunkFilename: "[id].css"
 		}),
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "index.html"
+		}),
 		new CleanWebpackPlugin(['dist']),
-		new NotifierPlugin({
-			onErrors: (severity, errors) => {
-				if (severity !== 'error') {
-					return;
-				}
-				const error = errors[0];
-				notifier.notify({
-					title: "Webpack error",
-					message: severity + ': ' + error.name,
-					subtitle: error.file || '',
-				});
-			}
-		})
-	]
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		quiet: true,
+		overlay: true
+	},
+	// optimization: {
+	// 	minimizer: [
+	// 		new TerserPlugin(),
+	// 		new OptimizeCSSAssetsPlugin({ cssProcessorOptions: { map: { inline: false, annotation: true}}})
+	// 	]
+	// },
+	// module: {
+	// 	rules: [
+	// 		{
+	// 			test: /\.html$/,
+	// 			use: [{ loader: "html-loader", options: { minimize: true } }]
+	// 		},
+	// 		{
+	// 			test: /\.(jpe?g|png|gif|svg)$/i,
+	// 			loader: "url-loader",
+	// 		},
+	// 		{
+	// 			test: /\.m?js$/,
+	// 			use: {
+	// 				loader: 'babel-loader',
+	// 				options: {
+	// 					presets: ['@babel/preset-env']
+	// 				}
+	// 			}
+	// 		},
+	// 		{
+	// 			test: /\.scss$/,
+	// 			use: [
+	// 				{loader: "style-loader"},
+	// 				{loader: MiniCssExtractPlugin.loader},
+	// 				{loader: "css-loader"},
+	// 				{loader: "postcss-loader", options: {
+	// 						sourceMap: true,
+	// 						plugins: () => [
+	// 							require('autoprefixer')({
+	// 								'browsers': ['> 0.2%', 'last 100 versions']
+	// 							}),
+	// 							require('css-mqpacker'),
+	// 							require('cssnano')({
+	// 								preset: [
+	// 									'default',
+	// 									{
+	// 										discardComments: {
+	// 											removeAll: true
+	// 										}
+	// 									}
+	// 								]
+	// 							})]
+	// 					}},
+	// 				{loader: "sass-loader"},
+	//
+	// 			]
+	// 		}
+	// 	]
+	// },
+	// plugins: [
+	// 	new HtmlWebPackPlugin({
+	// 		template: "src/index.html",
+	// 		filename: "./index.html"
+	// 	}),
+	// 	new MiniCssExtractPlugin({
+	// 		filename: "[name].css",
+	// 		chunkFilename: "[id].css"
+	// 	}),
+	// 	new CleanWebpackPlugin(['dist']),
+	// 	new NotifierPlugin({
+	// 		onErrors: (severity, errors) => {
+	// 			if (severity !== 'error') {
+	// 				return;
+	// 			}
+	// 			const error = errors[0];
+	// 			notifier.notify({
+	// 				title: "Webpack error",
+	// 				message: severity + ': ' + error.name,
+	// 				subtitle: error.file || '',
+	// 			});
+	// 		}
+	// 	})
+	// ]
 };
